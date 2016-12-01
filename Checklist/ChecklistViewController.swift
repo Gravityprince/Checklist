e9f75a521f0985f84d473d4bff64977fbddf62a0
Paul Tader <ptader@circletee.com>
2016-12-01 08:18:17 -0600

    Added viewWithTag and setup static data for rows

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -22,14 +22,28 @@ class ChecklistViewController: UITableViewController {
   
   // The tableView object from the tableViewController (added to the storyboard) calls these functions.
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    print("Running tableView(numberOfRowsInSection)")
+    return 5
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    print("Running tableView(cellForRowAtIndex)")
     let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-    return cell
+  
+  let label = cell.viewWithTag(1000) as! UILabel
+  
+  if indexPath.row == 0 {
+    label.text = "Walk the dog"
+  } else if indexPath.row == 1 {
+    label.text = "Brush my teeth"
+  } else if indexPath.row == 2 {
+    label.text = "Learn iOS development"
+  } else if indexPath.row == 3 {
+    label.text = "Soccor practice"
+  } else if indexPath.row == 4 {
+    label.text = "Eat ice cream"
+  }
+  return cell
   }
 
-
 }
-
