99d6cdc44a42a9bcde0260b0dc4d240f205182c9
Paul Tader <ptader@circletee.com>
2016-12-03 11:54:15 -0600

    Added configureCheckmark func

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -34,14 +34,12 @@ class ChecklistViewController: UITableViewController {
   
   // The tableView object from the tableViewController (added to the storyboard) calls these functions.
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    print("Running tableView(numberOfRowsInSection)")
     return 5
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    print("Running tableView(cellForRowAtIndex) for row: \(indexPath.row)")
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-      
+  
+  let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
   let label = cell.viewWithTag(1000) as! UILabel
     
   if indexPath.row == 0 {
@@ -55,7 +53,8 @@ class ChecklistViewController: UITableViewController {
   } else if indexPath.row == 4 {
     label.text = row4text
   }
-    
+  
+  configureCheckmark(for: cell, at: indexPath)
   return cell
   }
   
@@ -93,5 +92,27 @@ class ChecklistViewController: UITableViewController {
     tableView.deselectRow(at: indexPath, animated: true)
   }
   
+  func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath) {
+    var isChecked = false
+    
+    if indexPath.row == 0 {
+      isChecked = row0checked
+    } else if indexPath.row == 1 {
+      isChecked = row1checked
+    } else if indexPath.row == 2 {
+      isChecked = row2checked
+    } else if indexPath.row == 3 {
+      isChecked = row3checked
+    } else if indexPath.row == 4 {
+      isChecked = row4checked
+    }
+    
+    if isChecked {
+      cell.accessoryType = .checkmark
+    } else {
+      cell.accessoryType = .none
+    }
+    
+  }
   
 }
