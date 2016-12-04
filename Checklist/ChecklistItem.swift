f25f2e65e45fe2692850b76e3fbecacb0fca7e11
Paul Tader <ptader@circletee.com>
2016-12-04 15:57:43 -0600

    Created the ChecklistItem object

---
+++ Checklist/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklist
+//
+//  Created by Paul Tader on 12/4/16.
+//  Copyright © 2016 CircleTee. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -9,18 +9,35 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
-  var row0text = "Walk the dog"
-  var row1text = "Brush teeth"
-  var row2text = "Learn iOS development"
-  var row3text = "Guitar practice"
-  var row4text = "Learn Swift"
+  var row0item: ChecklistItem
+  var row1item: ChecklistItem
+  var row2item: ChecklistItem
+  var row3item: ChecklistItem
+  var row4item: ChecklistItem
   
-  var row0checked = false
-  var row1checked = false
-  var row2checked = false
-  var row3checked = false
-  var row4checked = false
-  
+  required init?(coder aDecoder: NSCoder){
+    row0item = ChecklistItem()
+    row0item.text = "Walk the Dog"
+    row0item.checked = false
+    
+    row1item = ChecklistItem()
+    row1item.text = "Brush my teeth"
+    row1item.checked = false
+
+    row2item = ChecklistItem()
+    row2item.text = "Learn iOS development"
+    row2item.checked = true
+    
+    row3item = ChecklistItem()
+    row3item.text = "Soccer practice"
+    row3item.checked = false
+    
+    row4item = ChecklistItem()
+    row4item.text = "Eat ice cream"
+    row4item.checked = true
+    
+    super.init(coder: aDecoder)
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -43,15 +60,15 @@ class ChecklistViewController: UITableViewController {
   let label = cell.viewWithTag(1000) as! UILabel
     
   if indexPath.row == 0 {
-    label.text = row0text
+    label.text = row0item.text
   } else if indexPath.row == 1 {
-    label.text = row1text
+    label.text = row1item.text
   } else if indexPath.row == 2 {
-    label.text = row2text
+    label.text = row2item.text
   } else if indexPath.row == 3 {
-    label.text = row3text
+    label.text = row3item.text
   } else if indexPath.row == 4 {
-    label.text = row4text
+    label.text = row4item.text
   }
   
   configureCheckmark(for: cell, at: indexPath)
@@ -63,15 +80,15 @@ class ChecklistViewController: UITableViewController {
       
       // Reverse the checkmark accessory...
       if indexPath.row == 0 {
-        row0checked = !row0checked
+        row0item.checked = !row0item.checked
       } else if indexPath.row == 1 {
-        row1checked = !row1checked
+        row1item.checked = !row1item.checked
       } else if indexPath.row == 2 {
-        row2checked = !row2checked
+        row2item.checked = !row2item.checked
       } else if indexPath.row == 3 {
-        row3checked = !row3checked
+        row3item.checked = !row3item.checked
       } else if indexPath.row == 4 {
-        row4checked = !row4checked
+        row4item.checked = !row4item.checked
       }
     
     configureCheckmark(for: cell, at: indexPath)
@@ -84,15 +101,15 @@ class ChecklistViewController: UITableViewController {
     var isChecked = false
     
     if indexPath.row == 0 {
-      isChecked = row0checked
+      isChecked = row0item.checked
     } else if indexPath.row == 1 {
-      isChecked = row1checked
+      isChecked = row1item.checked
     } else if indexPath.row == 2 {
-      isChecked = row2checked
+      isChecked = row2item.checked
     } else if indexPath.row == 3 {
-      isChecked = row3checked
+      isChecked = row3item.checked
     } else if indexPath.row == 4 {
-      isChecked = row4checked
+      isChecked = row4item.checked
     }
     
     if isChecked {
