935c3a5f75e6caa4c5e8f34412be433c389a7ea1
Paul Tader <ptader@circletee.com>
2016-12-07 08:29:01 -0600

    Added viewWillAppear(). Done button on keyboard disabled until text.

--- Checklist/AddItemViewController.swift
+++ Checklist/AddItemViewController.swift
@@ -16,13 +16,22 @@ class AddItemViewController: UITableViewController {
   }
   
   @IBAction func done() {
+    print("Contents of the text field: \(textField.text!)")
     dismiss(animated: true, completion: nil)
   }
+  
+  @IBOutlet weak var textField: UITextField!
+  
   
   // Delegate send "nil" back when the table view send willSelectRowAt to
   // disable the row selection and the row turning grey.
   override func tableView(_ tableView: UITableView, willSelectRowAt indexPAth: IndexPath) -> IndexPath? {
     return nil
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
   }
   
 
