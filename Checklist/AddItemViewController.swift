6c0524b2dab6c4cf94f0b72d203729f601238de9
Paul Tader <ptader@circletee.com>
2016-12-07 17:51:52 -0600

    Set the AddItemViewController to become a UITextFieldDelegate and implement textField(shouldChangeCharactersIn, replacementString)

--- Checklist/AddItemViewController.swift
+++ Checklist/AddItemViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
   
   @IBAction func cancel(){
     dismiss(animated: true, completion: nil)
@@ -21,6 +21,7 @@ class AddItemViewController: UITableViewController {
   }
   
   @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
   
   
   // Delegate send "nil" back when the table view send willSelectRowAt to
@@ -31,8 +32,18 @@ class AddItemViewController: UITableViewController {
   
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
-    textField.becomeFirstResponder()
   }
   
+  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+                 replacementString string: String) -> Bool {
+    
+    // We want to use NSString's replacingCharacters method so we "convert" Swifts
+    // String into a NSString.
+    let oldText = textField.text! as NSString
+    let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+    doneBarButton.isEnabled = (newText.length > 0)
+
+    return true
+  }
 
 }
