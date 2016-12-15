cf8000b934df8cf4e3ce026ea61726e6b9689ff7
Paul Tader <ptader@circletee.com>
2016-12-15 17:28:22 -0600

    Code refactor. AddItemViewController -> ItemDetailViewController

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ChecklistViewController: UITableViewController, AddItemViewControllerDelegate {
+class ChecklistViewController: UITableViewController, ItemDetailViewControllerDelegate {
   
   var items: [ChecklistItem]
   
@@ -104,12 +104,12 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     label.text = item.text
   }
   
-  // addItemViewControllerDidCancel protocol methods
-  func addItemViewControllerDidCancel(_ controller: AddItemViewController) {
+  // itemDetailViewControllerDidCancel protocol methods
+  func itemDetailViewControllerDidCancel(_ controller: ItemDetailViewController) {
     dismiss(animated: true, completion: nil)
   }
   
-  func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem){
+  func itemDetailViewController(_ controller: ItemDetailViewController, didFinishAdding item: ChecklistItem){
     let newRowIndex = items.count
     items.append(item)
     
@@ -122,7 +122,7 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
     dismiss(animated: true, completion: nil)
   }
   
-  func addItemViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem){
+  func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem){
     if let index = items.index(of: item) {
       let indexPath = IndexPath(row: index, section: 0)
       if let cell = tableView.cellForRow(at: indexPath) {
@@ -136,11 +136,11 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
   override func prepare(for segue: UIStoryboardSegue, sender: Any?){
     if segue.identifier == "AddItem"{
       let navigationController = segue.destination as! UINavigationController
-      let controller = navigationController.topViewController as! AddItemViewController
+      let controller = navigationController.topViewController as! ItemDetailViewController
       controller.delegate = self
     } else if segue.identifier == "EditItem" {
       let navigationController = segue.destination as! UINavigationController
-      let controller = navigationController.topViewController as! AddItemViewController
+      let controller = navigationController.topViewController as! ItemDetailViewController
       controller.delegate = self
       
       // We need the row number in oder to send it to the editItem
--- Checklist/ItemDetailViewController.swift
+++ Checklist/ItemDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  AddItemViewController.swift
+//  ItemDetailViewController.swift
 //  Checklist
 //
 //  Created by Paul Tader on 12/6/16.
@@ -9,36 +9,36 @@
 import Foundation
 import UIKit
 
-// This defines the AddItemViewControllerDelegate protocol. Note
+// This defines the ItemDetailViewControllerDelegate protocol. Note
 // functions with out code.
-protocol AddItemViewControllerDelegate: class {
-  func addItemViewControllerDidCancel(_ controller: AddItemViewController)
-  func addItemViewController(_ controller: AddItemViewController, didFinishAdding item: ChecklistItem)
-  func addItemViewController(_ controller: AddItemViewController, didFinishEditing item: ChecklistItem)
+protocol ItemDetailViewControllerDelegate: class {
+  func itemDetailViewControllerDidCancel(_ controller: ItemDetailViewController)
+  func itemDetailViewController(_ controller: ItemDetailViewController, didFinishAdding item: ChecklistItem)
+  func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem)
 }
 
-class AddItemViewController: UITableViewController, UITextFieldDelegate {
+class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
   
-  // The "delegate" variable is set by the calling controller, a AddItemViewControllerDelegate
+  // The "delegate" variable is set by the calling controller, a ItemDetailViewControllerDelegate
   // and then used throughout the code here. This is the connection back (through the portal)
-  weak var delegate: AddItemViewControllerDelegate?
+  weak var delegate: ItemDetailViewControllerDelegate?
   
   // A place to recieve the incoming ChecklistItem to edit.
   var itemToEdit: ChecklistItem?
   
   @IBAction func cancel(){
-    delegate?.addItemViewControllerDidCancel(self)
+    delegate?.itemDetailViewControllerDidCancel(self)
   }
   
   @IBAction func done() {
     if let item = itemToEdit {
       item.text = textField.text!
-      delegate?.addItemViewController(self, didFinishEditing: item)
+      delegate?.itemDetailViewController(self, didFinishEditing: item)
     } else {
       let item = ChecklistItem()
       item.text = textField.text!
       item.checked = false
-      delegate?.addItemViewController(self, didFinishAdding: item)
+      delegate?.itemDetailViewController(self, didFinishAdding: item)
     }
 
   }
