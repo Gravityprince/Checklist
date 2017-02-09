4fc198d31346719fbc5e58751e0330ee573feef8
Paul Tader <ptader@circletee.com>
2017-02-09 08:23:17 -0600

    Added coder/decoder code for new Checklist Item vars

--- Checklist/ChecklistItem.swift
+++ Checklist/ChecklistItem.swift
@@ -11,6 +11,9 @@ import Foundation
 class ChecklistItem: NSObject, NSCoding {
   var text = ""
   var checked = false
+  var dueDate = Date()
+  var shouldRemind = false
+  var itemID: Int
 
   func toggleChecked() {
     checked = !checked
@@ -23,12 +26,18 @@ class ChecklistItem: NSObject, NSCoding {
   required init?(coder aDecoder: NSCoder){
     text = aDecoder.decodeObject(forKey: "Text") as! String
     checked = aDecoder.decodeBool(forKey: "Checked")
+    dueDate = aDecoder.decodeObject(forKey: "DueDate") as! Date
+    shouldRemind = aDecoder.decodeBool(forKey: "ShouldRemind")
+    itemID = aDecoder.decodeInteger(forKey: "ItemID")
     super.init()
   }
   
   func encode(with aCoder: NSCoder) {
     aCoder.encode(text, forKey: "Text")
     aCoder.encode(checked, forKey: "Checked")
+    aCoder.encode(dueDate, forKey: "DueDate")
+    aCoder.encode(shouldRemind, forKey: "ShouldRemind")
+    aCoder.encode(itemID, forKey: "ItemID")
   }
 
 }
